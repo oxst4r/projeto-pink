3de23e1e6efcb9b979dda2429f8233bfc7d217d8
oxst4r <esterccds@gmail.com>
2024-09-03 13:59:29 -0300

    salvando

---
+++ sketch.js
@@ -0,0 +1,50 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);     
+  }
+  function draw() {
+      background("white");
+      
+      rect(0, 10, 100, 150);
+  }
+  function draw() {
+      background("white");
+  
+      fill("pink");
+      rect(0, 10, 100, 150);
+  }
+  function draw() {
+      background("white");
+      
+      stroke("black") 
+      fill("pink") 
+      rect(0, 10, 100, 150)
+  }
+  function draw() {
+    background(220);
+    
+    stroke("black");
+    fill("pink");
+    rect(mouseX, mouseY, 100, 150);
+  }
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("black");
+    fill("pink");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
